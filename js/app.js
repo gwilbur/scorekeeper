791dfdc7940e129091d36c4d27fd16c81705ce2a
Greg Wilbur <gwilbur@gmail.com>
2017-10-17 20:15:30 -0400

    Use a table, text at the corners

--- js/app.js
+++ js/app.js
@@ -1,19 +1,34 @@
 Vue.component('play-score', {
   template: `
     <div class="play-score">
-      <div v-for="(text, index) in playScoreText"
-        class="play-score-text"
-        :class="'play-score-text-' + index"
-      >
-        {{ text }}
-      </play-score-text>
+      <table>
+        <tr>
+          <td>{{ topLeftText }}</td>
+          <td>{{ topRightText }}</td>
+        </tr>
+        <tr>
+          <td>{{ bottomLeftText }}</td>
+          <td>{{ bottomRightText }}</td>
+        </tr>
+      </table>
     </div>`,
-  props: ['playScoreText']
+  props: {
+    topLeftText: String,
+    topRightText: String,
+    bottomLeftText: String,
+    bottomRightText: String
+  }
 });
 
 let app = new Vue({
   el: '#app',
-  data: {
-    sampleText: ['1B', 'SB', 'WP', 'CS']
-  }
+  template: `
+    <div id="app">
+      <play-score
+        topLeftText="WP"
+        topRightText="SB"
+        bottomLeftText="CS"
+        bottomRightText="1B"
+      />
+    </div>`
 });
