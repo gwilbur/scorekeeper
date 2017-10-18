6f600bb88a1de556a41e0e394ba17dc868af6bb7
Greg Wilbur <gwilbur@gmail.com>
2017-10-18 01:27:37 -0400

    short stack font and center text

--- js/app.js
+++ js/app.js
@@ -1,22 +1,18 @@
 Vue.component('play-score', {
   template: `
     <div class="play-score">
+      <div class="center-text">{{ centerText }}</div>
       <table>
-        <tr>
-          <td>{{ topLeftText }}</td>
-          <td>{{ topRightText }}</td>
-        </tr>
-        <tr>
-          <td>{{ bottomLeftText }}</td>
-          <td>{{ bottomRightText }}</td>
-        </tr>
+        <tr><td>{{ topLeftText }}</td><td>{{ topRightText }}</td></tr>
+        <tr><td>{{ bottomLeftText }}</td><td>{{ bottomRightText }}</td></tr>
       </table>
     </div>`,
   props: {
-    topLeftText: String,
-    topRightText: String,
-    bottomLeftText: String,
-    bottomRightText: String
+    centerText: {type: String, default: ''},
+    topLeftText: {type: String, default: ''},
+    topRightText: {type: String, default: ''},
+    bottomLeftText: {type: String, default: ''},
+    bottomRightText: {type: String, default: ''}
   }
 });
 
@@ -25,6 +21,7 @@ let app = new Vue({
   template: `
     <div id="app">
       <play-score
+        centerText="6-4-3"
         topLeftText="WP"
         topRightText="SB"
         bottomLeftText="CS"
